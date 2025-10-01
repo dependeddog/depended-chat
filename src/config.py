da20dd641d3aed0043abce4977ddda5d577774bd
kasan <kasanindaniil@gmail.com>
2025-10-01 14:27:20 +1000

    feature: move all params from config to .env file

--- src/config.py
+++ src/config.py
@@ -16,20 +16,37 @@ class Settings(BaseSettings):
         validation_alias=AliasChoices("JWT_SECRET_KEY", "JWT_SECRET"),
         description="Секрет для подписи JWT",
     )
+    jwt_algorithm: str = Field(
+        ...,
+        validation_alias="JWT_ALGORITHM"
+    )
+    access_token_expire_minutes: int = Field(
+        ...,
+        validation_alias="ACCESS_TOKEN_EXPIRE_MINUTES"
+    )
+    refresh_token_expire_days: int = Field(
+        ...,
+        validation_alias="REFRESH_TOKEN_EXPIRE_DAYS"
+    )
+    jwt_issuer: str = Field(
+        ...,
+        validation_alias="JWT_ISSUER"
+    )
+    jwt_audience: str = Field(
+        ...,
+        validation_alias="JWT_AUDIENCE"
+    )
+
     database_url: str = Field(
         ...,
         validation_alias=AliasChoices("DATABASE_URL", "DB_URL"),
-        description="DSN Postgres, например: postgresql+asyncpg://user:pass@host:5432/db",
+        description="DSN Postgres",
     )
 
-    # Опциональные/имеют дефолты
-    jwt_algorithm: str = Field("HS256", validation_alias="JWT_ALGORITHM")
-    access_token_expire_minutes: int = Field(15, validation_alias="ACCESS_TOKEN_EXPIRE_MINUTES")
-    refresh_token_expire_days: int = Field(14, validation_alias="REFRESH_TOKEN_EXPIRE_DAYS")
-    jwt_issuer: str = Field("depended-chat", validation_alias="JWT_ISSUER")
-    jwt_audience: str = Field("depended-users", validation_alias="JWT_AUDIENCE")
-
-    rabbitmq_url: str = Field("amqp://guest:guest@localhost/", validation_alias="RABBITMQ_URL")
+    rabbitmq_url: str = Field(
+        ...,
+        validation_alias="RABBITMQ_URL"
+    )
 
     @property
     def jwt_secret(self) -> str:
