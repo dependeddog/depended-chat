f2504df1f1e00fc9233dba51b5f6504177355720
Honfi555 <kasanindaniil@gmail.com>
2026-01-14 15:22:10 +1000

    refactor (chat api): remove unnecessary import

--- src/chat/router.py
+++ src/chat/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
